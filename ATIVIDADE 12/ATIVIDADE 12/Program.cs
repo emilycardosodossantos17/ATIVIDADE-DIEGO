fccb862916cb1eb7a1e84fd16675a63c2d159991
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 16:00:27 -0300

    Add files via upload

---
+++ ATIVIDADE 12/ATIVIDADE 12/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Digite o pimeiro valor: ")
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite o segndo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+double maior = (a > b) ? a : b;
+
+Console.WriteLine($"O maior valor é: {maior}");
+Console.WriteLine("Pressione Enter para sair...");
+Console.WriteLine();
