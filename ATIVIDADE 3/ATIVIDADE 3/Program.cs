35827d289656f91f256da81416b288ed38df732f
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 17:17:32 -0300

    Add files via upload

---
+++ ATIVIDADE 3/ATIVIDADE 3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Digite a base do retangulo: ");
+double b = double.`Pearse(console.ReadLine());
+
+Console.Write("Digite a altura do retangulo: ");
+double h = double.Pearse(Console.ReadLine());
+
+double area = b * h;
+
+Console.WriteLine($"A area do retangulo é: {area}");
