2713b90ed47b3eb21e6126c32d74a29546f9c2e0
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 15:16:51 -0300

    Add files via upload

---
+++ ATIVIDADE 13/ATIVIDADE 13/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Digite o prmeiro valor: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite o segundo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite o terceiro valor: ");
+double c = double.Parse(Console.ReadLine() ?? "0");
+
+double[] arr = new double[] { a, b, c };
+Array.Sort(arr);
+
+Console.WriteLine("valores em ordem crescente");
+Console.WriteLine($"{arr[0]}, {arr[1]}, {arr[2]}");
+Console.WriteLine("Pressione Enter para sair..");
+Console.ReadLine();
