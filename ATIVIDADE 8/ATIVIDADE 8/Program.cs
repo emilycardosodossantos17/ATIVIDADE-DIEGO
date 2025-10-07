acae6bcb08044f03eb37a3afac61fd3231cff7b5
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 16:57:58 -0300

    Add files via upload

---
+++ ATIVIDADE 8/ATIVIDADE 8/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Digite um valor: ");
+double valor = double.Parse(Console.ReadLine());
+
+if (valor >10)
+    Console.WriteLine("É MAIOR QUE 10!");
+
+Console.WriteLine(NÃO É MAOIR QUE 10!");
+    
