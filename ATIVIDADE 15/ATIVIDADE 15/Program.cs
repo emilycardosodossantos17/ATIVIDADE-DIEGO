80bb02582a736231e2c5e75ae6f08db0f7a13a5d
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 14:44:26 -0300

    Add files via upload

---
+++ ATIVIDADE 15/ATIVIDADE 15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("numero entre 1000 e 2000 que deixam resto 5 na divisã por 11:")
+
+for (int i = <= 2000; i++)
+
+    if (i % 11 == 5)
+
+        Console.WriteLine(i);
+
+
+Console.WriteLine("nPressine Enter para sair...");
+ Console.WriteLine();
+
+
