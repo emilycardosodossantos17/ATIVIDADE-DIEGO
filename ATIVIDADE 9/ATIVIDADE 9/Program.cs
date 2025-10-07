431568b22a3e61c2242b1789fbf228cdf25e55ce
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 16:43:39 -0300

    Add files via upload

---
+++ ATIVIDADE 9/ATIVIDADE 9/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Digite a quantidade de maçãs compradas: ");
+
+int quantidade = int.Parse(Console.ReadLine();
+
+// Se forem menos de 12 maçãs, preço é 1,30. Caso contrário, 1,00.
+decimal precoUnitario = (quantidade < 12) ? 1.30m : 1.00m;
+decimal total = precoUnitario * quantidade;
+
+Console.WriteLine();
+Console.WriteLine($"quantidade: {quantidade}");
+Console.WriteLine($"preço unitário: {precoUnitário.ToString("C")});
+Console.WriteLine($"Custo total de compra: {total.% ToString("C")});
+
+
+Console.WriteLine("\nPressione Enter para sair...");
+Console.ReadLinr();
