1f2ffe0cb089c3ec58ae789d5c680cb7f93bc931
emilycardosodossantos17 <emilycardosodossantos17@gmail.com>
2025-10-07 14:43:29 -0300

    Add files via upload

---
+++ ATIVIDADE 14/ATIVIDADE 14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um valor N (maior que 0): ");
+int N =.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"/nNumeros de 1 ate {N} :");
+for (int i = 1; i <= N; 1 = ++)
+
+    Console.WriteLine(1);
+
+Console.WriteLine("/nPressione Enter para sair...");
+Console.ReadLine();
+
